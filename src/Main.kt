932e4871ce9839212df7c072550399f4dfd1e9fa
Bernard <benbaah104@gmail.com>
2024-10-30 01:03:13 -0700

    sets and hashmaps

--- src/Main.kt
+++ src/Main.kt
@@ -55,4 +55,65 @@ fun main() {
     println(r)
 
 
+    // kotlin hashmap
+    val theMap = HashMap<String, Int>()
+    theMap["apple"] = 4
+    theMap["pawpaw"] = 5
+    theMap["grapes"] = 2
+    println(theMap)
+
+    // using pair
+    val theMap2 = mapOf(Pair("one", 1), Pair("two", 2), Pair("ten", 10), Pair("five", 5))
+    println(theMap2)
+
+    // Kotlin map Properties
+    println("Entries " + theMap.entries)
+    println("Keys " + theMap.keys)
+    println("Values " + theMap.values)
+
+    // using iterator to map through the map
+    val itr = theMap2.keys.iterator()
+    while(itr.hasNext()){
+        val key = itr.next()
+        val value = theMap2[key]
+        println("key = ${key} value = ${value}")
+    }
+
+    // using for loop
+    for((k, v)  in theMap2){
+        println("${k} = ${v}")
+    }
+
+    // using forEach
+
+    theMap2.forEach {k , v -> println("k = ${k} V = ${v}")}
+
+    // containsKey and value
+    if(theMap2.containsKey("one") && theMap2.containsValue(2)) {
+        println(true)
+    }
+
+    // removing elements from a mutable map
+    theMap.remove("apple")
+    println("result after romoving an element ${theMap}")
+
+    // sorting map
+    val RSortedMap = theMap2.toSortedMap()
+    println("Sorted Map $RSortedMap")
+
+    // filtering keys and values
+    val resultMap1 = theMap2.filterKeys { it != "five" }
+    println("filtered Keys ${resultMap1}")
+    val resultMap2 = theMap2.filterValues { it > 1 }
+    println("filtered values ${resultMap2}")
+
+    val resultMap3 = theMap.map{(v, k)-> "key is $k value is $v"}
+    println("Mapped object $resultMap3")
+
+    // mutable map
+    val theMap3 = mutableMapOf(Pair("Benz", 2), Pair("Chevez", 4), Pair("Ford", 4), Pair("Roseroyce", 3))
+    theMap3.put("Lambourghini", 1)
+    println(theMap3)
+    theMap3.remove("Chevez")
+    println("One Car is sold out from $theMap3")
 }
